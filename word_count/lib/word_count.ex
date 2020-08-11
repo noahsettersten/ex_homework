1a30ef7472a01b3fb372c5dbcb0e476b2313702e
Noah Settersten <noah@headway.io>
2020-08-11 13:53:56 -0500

    Complete word count exercise

--- word_count/lib/word_count.ex
+++ word_count/lib/word_count.ex
@@ -22,12 +22,56 @@ defmodule WordCount do
   """
 
   def run(file) do
-    file
-    |> read()
-    |> parse()
-    |> sum_words()
-    |> top10()
+    output = file
+             |> read()
+             |> parse()
+             |> sum_words()
+             |> top10()
+
+    IO.inspect(output)
   end
 
-  defp read(file), do: File.read(file)
+  defp read(file) do
+    case File.read(file) do
+      {:ok, text} -> {:ok, text}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp parse({:ok, contents}) do
+    words = String.downcase(contents)
+            |> String.replace(".", "")
+            |> String.split([" ", "\n"])
+            |> Enum.filter(fn(element) -> String.length(element) > 0 end)
+
+    {:ok, words}
+  end
+  defp parse(error), do: error
+
+  defp sum_words({:ok, words}) do
+    occurences = Enum.reduce(words, %{}, fn(x, acc) ->
+      Map.update(acc, x, 1, &(&1 + 1))
+    end)
+
+    {:ok, occurences}
+  end
+  defp sum_words(error), do: error
+
+  defp top10({:ok, words}) do
+    second_value = fn(element) ->
+      {_, count} = element
+
+      count
+    end
+
+    words
+    |> Enum.sort_by(second_value, :desc)
+    |> Enum.take(10)
+  end
+  defp top10({:error, error}) do
+    case error do
+      :enoent -> "File not found"
+      _ -> error
+    end
+  end
 end
